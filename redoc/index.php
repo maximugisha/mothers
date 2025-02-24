e147537a3c7272ce6fdfdcd4580919d67c9612c2
Maximo Mugisha <maximugisha@gmail.com>
2025-02-24 10:19:33 +0300

    add redoc

---
+++ redoc/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Church Management API Documentation</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.css" />
+  </head>
+  <body>
+    <redoc spec-url="openapi.yaml"></redoc>
+    <script src="https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"></script>
+  </body>
+</html>
